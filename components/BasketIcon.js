1b0697fbac10e5c3f2ffe54ac97184fb0f3cc9dc
Min Khant Kyaw <minkhantkyaw@cloudsource.co.jp>
2023-06-16 14:34:20 +0630

    data bind and basket total

---
+++ components/BasketIcon.js
@@ -0,0 +1,25 @@
+import {Text, TouchableOpacity, View} from "react-native";
+import {useSelector} from "react-redux";
+import {selectBasketItems, selectBasketTotal} from "../features/basketSlice";
+import {useNavigation} from "@react-navigation/native";
+import Currency from "react-currency-formatter";
+
+const BasketIcon = () => {
+    const items = useSelector(selectBasketItems);
+    const navigation = useNavigation();
+    const basketTotal = useSelector(selectBasketTotal);
+
+    return (
+        <View className="absolute bottom-10 w-full z-50">
+            <TouchableOpacity className="mx-5 bg-[#00CCBB] rounded-lg p-4 flex-row items-center space-x-1">
+                <Text className="text-lg text-white font-extrabold bg-[#01A2B6] py-1 px-3">{items.length}</Text>
+                <Text className="text-lg font-extrabold text-white flex-1 text-center">View Basket</Text>
+                <Text className="text-lg text-white font-extrabold">
+                    <Currency quantity={basketTotal} currency="MMK"/>
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default BasketIcon;
